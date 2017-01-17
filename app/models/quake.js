3163606760d919a0a1a3aab9180d4468fc1b1c2c
Philip Crotwell <crotwell@seis.sc.edu>
2017-01-17 12:50:01 -0500

    add numSuccessfulStations to event list output

--- app/models/quake.js
+++ app/models/quake.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  numSuccessfulStations: DS.attr('number'),
   prefOrigin: DS.belongsTo('origin', { async: true }),
   prefMagnitude: DS.belongsTo('magnitude', { async: false }),
   sodStatus: DS.attr('string'),
