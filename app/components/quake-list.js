f661bd47532e9ff333e9b979e7305d5fd1dffe7a
Philip Crotwell <crotwell@seis.sc.edu>
2017-01-20 14:32:58 -0500

    sortable quake list

--- app/components/quake-list.js
+++ app/components/quake-list.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortedQuakes: Ember.computed.sort('quakes', 'sortDefinition'),
+  sortDefinition: ['prefOrigin.time'],
   newestFirst: function() {
     return this.get('quakes').toArray().reverse();
-  }.property('quakes')
+  }.property('quakes'),
+  actions: {
+    sortBy: function(prop) {
+      let direction = "asc";
+      if (this.get('sortDefinition')[0].startsWith(prop)) {
+        if (this.get('sortDefinition')[0].endsWith(":desc")) {
+          direction = "asc";
+        } else {
+          direction = "desc";
+        }
+      } else {
+        direction = "asc";
+      }
+      this.set("sortDefinition", [ prop+":"+direction ]);
+    }
+  }
 });
