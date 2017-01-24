4d04bd7f09a3322ff0f7e82490b7f30ddc7a2be6
Philip Crotwell <crotwell@seis.sc.edu>
2017-01-24 16:25:30 -0500

    formatting of numbers with helper

--- app/components/raw-json-display.js
+++ app/components/raw-json-display.js
@@ -7,6 +7,11 @@ export default Ember.Component.extend({
     const rJ = this.get('rawJson');
     if (typeof rJ === 'string' || typeof rJ === 'number' || typeof rJ === 'boolean') {
       this.set('isSimple', true);
+      if (typeof rJ === 'number') {
+        this.set('isNumber', true);
+      } else {
+        this.set('isNumber', false);
+      }
       this.set('value', rJ);
       this.set('tagName', 'span');
     } else if (typeof rJ === 'object' && Array.isArray(rJ)) {
