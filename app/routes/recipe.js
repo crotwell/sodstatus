5bfe15af5c6a50c71c2f6795925107472b3239dd
Philip Crotwell <crotwell@seis.sc.edu>
2017-02-09 16:59:14 -0500

    switch to query to get current so id is ok

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('sodConfig', 'current'); 
+    return this.store.queryRecord('sodConfig', { current: true }); 
   }
 });
