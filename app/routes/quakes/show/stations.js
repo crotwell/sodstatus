15c77bef26942348eb3a173601f3b9df0665424c
Philip Crotwell <crotwell@seis.sc.edu>
2017-03-17 12:35:46 -0400

    add afterModel to force load of async relationships

--- app/routes/quakes/show/stations.js
+++ app/routes/quakes/show/stations.js
@@ -2,8 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var e = this.modelFor('quakes.show');
-    var s = e.get('esps');
-    return s;
+    return this.modelFor('quakes.show');
+  },
+  afterModel: function(model, transition) {
+    return Ember.RSVP.hash({
+      espsHash: model.get('esps'),
+      stationHash: Ember.RSVP.all(model.get('esps').getEach('station')),
+      latHash: Ember.RSVP.all(model.get('esps').getEach('station').getEach('latitude')),
+      netHash: Ember.RSVP.all(model.get('esps').getEach('station').getEach('network'))
+    });
   }
+
 });
