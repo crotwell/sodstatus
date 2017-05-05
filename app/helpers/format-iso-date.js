4cefb2aab2f33331f0099f973df28c071b03062f
Philip Crotwell <crotwell@seis.sc.edu>
2017-05-05 17:05:36 -0400

    blank if param undef

--- app/helpers/format-iso-date.js
+++ app/helpers/format-iso-date.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export function formatIsoDate(params, namedArgs) {
+  if (typeof params[0] == 'undefined') return "";
   let defaultFormat = 'YYYY-MM-DD[T]HH:mm:ss.SSSS';
   let date = params[0];
   let format = namedArgs.format===undefined ? defaultFormat : namedArgs.format;
--- app/helpers/format-number.js
+++ app/helpers/format-number.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export function formatNumber(params/*, hash*/) {
+  if ( typeof params[0] == 'undefined') { return "";}
   let decimalDigits = params[1]===undefined ? 2 : params[1];
   return params[0].toFixed(decimalDigits);
 }
