52406a7f6a81879eb904e38ced65089f8bd6ebbe
Philip Crotwell <crotwell@seis.sc.edu>
2017-05-22 09:23:28 -0400

    linting

--- app/components/quake-station-info.js
+++ app/components/quake-station-info.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   cookieNames: function() {
+    let out = [];
     for (let quakeVector in this.get('ecps')) {
       let cookies = quakeVector.get('cookieJar');
 console.log("cookieJar: "+cookies);
-      let out = [];
       for (var cName in cookies) {
-        if (cookieJar.hasOwnProperty(cName)) {
+        if (this.get('cookieJar').hasOwnProperty(cName)) {
           out.push(cName);
 console.log("cookie: "+cName);
         }
