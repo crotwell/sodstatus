16f47306c11572982ca3e761c3c494644175983e
Philip Crotwell <crotwell@seis.sc.edu>
2020-02-21 16:06:43 -0500

    starting over with clean ember new

--- app/router.js
+++ app/router.js
@@ -1,48 +1,10 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
-  this.route('arms');
-  this.route('networks', function() {
-    this.route('show', {
-      path: ':network_id'},
-      function() {
-        this.route('stations');
-      });
-    });
-  this.route('quakes', function() {
-        this.route('show', {
-          path: ':quake_id'},
-          function() {
-            this.route('stations');
-          });
-       });
-  this.route('station', 
-    {path: 'station/:station_id' },
-    function() {
-      this.route('quakes', function() {
-        this.route('show', function() {
-          this.route('stations');
-        });
-      });
-      this.route('quake',
-        {path: ':esp_id'});
-    });
-  this.route('channel',
-    {path: 'channel/:channel_id'});
-  this.route('recipe');
-  this.route('perusals', function() {
-    this.route('new');
-
-    this.route('show', {
-      path: ':perusal_id'
-    });
-  });
 });
-
-export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,10 @@
-/* eslint-env node */
+'use strict';
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
-    fingerprint: {
-      exclude: [
-        'images/layers-2x.png',
-        'images/layers.png',
-        'images/marker-icon-2x.png',
-        'images/marker-icon.png',
-        'images/marker-shadow.png'
-      ]
-    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -27,23 +19,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
-  app.import('vendor/fontello/fontello.css'); 
-  app.import('vendor/fontello/font/fontello.ttf', {
-    destDir: 'font'
-  });
-  app.import('vendor/fontello/font/fontello.eot', { 
-    destDir: 'font'
-  }); 
-  app.import('vendor/fontello/font/fontello.svg', {
-    destDir: 'font'
-  });
-  app.import('vendor/fontello/font/fontello.woff', { 
-    destDir: 'font'
-  });
-
-  app.import('bower_components/picnic/releases/plugins.min.css');
-  app.import('bower_components/picnic/releases/picnic.min.css');
 
   return app.toTree();
 };
